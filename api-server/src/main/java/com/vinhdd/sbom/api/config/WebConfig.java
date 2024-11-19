b74d42d55dce0bce81fe07a0ced8d3ed34924fc1
vinh412 <vinhduong412@gmail.com>
2024-11-19 15:12:05 +0700

    enable cors

--- api-server/src/main/java/com/vinhdd/sbom/api/config/WebConfig.java
+++ api-server/src/main/java/com/vinhdd/sbom/api/config/WebConfig.java
@@ -8,11 +8,21 @@ import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 @RequiredArgsConstructor
-public class ApplicationConfig {
+public class WebConfig implements WebMvcConfigurer {
     private final UserDetailsService userDetailsServiceImpl;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+
     @Bean
     public AuthenticationManager authenticationManager(){
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
